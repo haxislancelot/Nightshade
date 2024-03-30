7487451fe28abb124df6359ac608fb32d0e67e36
Nihil <84822175+haxislancelot@users.noreply.github.com>
2024-03-29 21:34:38 -0300

    Add files via upload

--- plugins/gapp.sh
+++ plugins/gapp.sh
@@ -6,43 +6,43 @@
 
 disable_menu() {
 clear
-echo -e "\e[44m\e[97m--- Gapps Disabler by haxislancelot @ Github ---\e[0m"
-sleep 0.1
+echo -e "${G}--- Gapps Disabler by haxislancelot @ Github ---${F}"
+sleep 0
 echo ""
-sleep 0.1
-echo "Disable Gapps [ 1 ]"
-sleep 0.1
-echo "Enable Gapps [ 2 ]"
-sleep 0.1
-echo "Exit [ 0 ]"
-sleep 0.1
+sleep 0
+echo "\033[0;90m[ - ] Disable Gapps [ 1 ]"
+sleep 0
+echo "[ - ] Enable Gapps [ 2 ]"
+sleep 0
+echo "[ - ] Return to plugins menu [ 0 ]"
+sleep 0
 echo ""
-sleep 0.1
-echo -n "Enter your choice: "
+sleep 0
+echo -n "${G}Enter your choice: ${F}"
 read choice
 
 case $choice in
     1)
         clear
-        pm disable com.google.android.googlequicksearchbox > /dev/null
-        pm disable com.android.chrome > /dev/null
-        pm disable com.google.android.apps.photos > /dev/null
-        pm disable com.google.android.apps.nbu.files > /dev/null
-        pm disable com.google.android.apps.safetyhub > /dev/null
-        pm disable com.google.android.apps.wellbeing > /dev/null
-        echo "Gapps disabled!"
+        pm disable com.google.android.googlequicksearchbox > /dev/null 2>&1
+        pm disable com.android.chrome > /dev/null 2>&1
+        pm disable com.google.android.apps.photos > /dev/null 2>&1
+        pm disable com.google.android.apps.nbu.files > /dev/null 2>&1
+        pm disable com.google.android.apps.safetyhub > /dev/null 2>&1
+        pm disable com.google.android.apps.wellbeing > /dev/null 2>&1
+        echo "${G}Gapps disabled!${F}"
         sleep 1
         disable_menu
         ;;
     2)
         clear
-        pm enable com.google.android.googlequicksearchbox > /dev/null
-        pm enable com.android.chrome > /dev/null
-        pm enable com.google.android.apps.photos > /dev/null
-        pm enable com.google.android.apps.nbu.files > /dev/null
-        pm enable com.google.android.apps.safetyhub > /dev/null
-        pm enable com.google.android.apps.wellbeing > /dev/null
-        echo "Gapps enabled!"
+        pm enable com.google.android.googlequicksearchbox > /dev/null 2>&1
+        pm enable com.android.chrome > /dev/null 2>&1
+        pm enable com.google.android.apps.photos > /dev/null 2>&1
+        pm enable com.google.android.apps.nbu.files > /dev/null 2>&1
+        pm enable com.google.android.apps.safetyhub > /dev/null 2>&1
+        pm enable com.google.android.apps.wellbeing > /dev/null 2>&1
+        echo "${G}Gapps enabled!${F}"
         sleep 1
         disable_menu        
         ;;
