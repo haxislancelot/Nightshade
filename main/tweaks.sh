4d237c0d1293e1a552ccca48ca9b39fbbda3230c
Nihil <84822175+haxislancelot@users.noreply.github.com>
2024-03-31 10:50:11 -0300

    Update tweaks.sh

--- main/tweaks.sh
+++ main/tweaks.sh
@@ -14,7 +14,6 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with Nightshade.  If not, see <https://www.gnu.org/licenses/>.
-#
 # Copyright (C) 2024 haxislancelot
 
 # Logs
