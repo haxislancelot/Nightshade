c5c85852532850c0cc6f7d1d72da397879e2be11
Nihil <84822175+haxislancelot@users.noreply.github.com>
2024-04-06 21:09:48 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -12,7 +12,6 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with Nightshade.  If not, see <https://www.gnu.org/licenses/>.
-#
 # Copyright (C) 2024 haxislancelot
 clear
 
