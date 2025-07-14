72c873d81b11615c75a0a5f3c9853b69dae28a32
Nihil <84822175+haxislancelot@users.noreply.github.com>
2025-07-14 07:21:51 -0300

    Some changes in s5e8825_balanced.
    Second attempt to fix underclocking on Big and Little CPU;
    
    Reverted to exynos-migov underclocking and I reset its permissions after applying underclocking.
     
    @donottellmyname

--- main/tweaks.sh
+++ main/tweaks.sh
@@ -1569,32 +1569,32 @@ s5e8825_balanced() {
     write "/sys/devices/system/cpu/cpu6/online" "1"
     write "/sys/devices/system/cpu/cpu7/online" "1"
     
-    write "/sys/devices/system/cpu/cpufreq_limit/cpufreq_max_limit" "2288000"
+    write "/sys/devices/platform/exynos-migov/cl0/cl0_pm_qos_max_freq" "2002000" # 2002000 default
     chown root /sys/devices/system/cpu/cpufreq/policy0/scaling_max_freq
-    write "/sys/devices/system/cpu/cpufreq/policy6/scaling_max_freq" "2288000"
+    write "/sys/devices/system/cpu/cpufreq/policy0/scaling_max_freq" "2002000" # 2002000 default
     
-    write "/sys/devices/system/cpu/cpufreq_limit/little_max_limit" "2002000" 
-    chown root /sys/devices/system/cpu/cpufreq/policy0/scaling_max_freq
-    write "/sys/devices/system/cpu/cpufreq/policy0/scaling_max_freq" "2002000"
+    write "/sys/devices/platform/exynos-migov/cl1/cl1_pm_qos_max_freq" "2288000" # 2288000 default
+    chown root /sys/devices/system/cpu/cpufreq/policy6/scaling_max_freq
+    write "/sys/devices/system/cpu/cpufreq/policy6/scaling_max_freq" "2288000" # 2288000 default
 
     chmod 0444 /sys/devices/system/cpu/cpufreq/policy*/scaling_max_freq
     
     # Maximum CPU frequency limit to save power
     chmod 644 /sys/devices/platform/exynos-ufcc/ufc/cpufreq_max_limit
     write "/sys/devices/platform/exynos-ufcc/ufc/cpufreq_max_limit" "2288000"
-    chmod 444 /sys/devices/platform/exynos-ufcc/ufc/cpufreq_max_limit
+    chmod 000 /sys/devices/platform/exynos-ufcc/ufc/cpufreq_max_limit
 
     chmod 644 /sys/devices/platform/exynos-ufcc/ufc/cpufreq_min_limit
     write "/sys/devices/platform/exynos-ufcc/ufc/cpufreq_min_limit" "533000"
-    chmod 444 /sys/devices/platform/exynos-ufcc/ufc/cpufreq_min_limit
+    chmod 000 /sys/devices/platform/exynos-ufcc/ufc/cpufreq_min_limit
 
     chmod 644 /sys/devices/platform/exynos-ufcc/ufc/little_max_limit
     write "/sys/devices/platform/exynos-ufcc/ufc/little_max_limit" "2002000"
-    chmod 444 /sys/devices/platform/exynos-ufcc/ufc/little_max_limit
+    chmod 000 /sys/devices/platform/exynos-ufcc/ufc/little_max_limit
 
     chmod 644 /sys/devices/platform/exynos-ufcc/ufc/little_min_limit
     write "/sys/devices/platform/exynos-ufcc/ufc/little_min_limit" "533000"
-    chmod 444 /sys/devices/platform/exynos-ufcc/ufc/little_min_limit
+    chmod 000 /sys/devices/platform/exynos-ufcc/ufc/little_min_limit
 
     simple_bar
     kmsg1 "[*] CPU UNDERCLOCKED. "
